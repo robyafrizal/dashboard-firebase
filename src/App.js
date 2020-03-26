b484fccd45c73ab43618517a238793c5f18f8acf
robyafrizal <robyafrizal86@gmail.com>
2020-03-26 23:29:49 +0700

    Add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome Roby</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Welcome Roby</h1>
+      </div>
+    </Router>
   );
 }
 
